aa3c10a06f48cacf53ae50a98dd09a6968d695da
Joel Jonsson <joel.jonsson@smartbear.com>
2016-04-15 16:25:22 +0200

    First release!

--- src/main/java/com/smartbear/ready/plugin/discovery/har/PluginConfig.java
+++ src/main/java/com/smartbear/ready/plugin/discovery/har/PluginConfig.java
@@ -6,6 +6,6 @@ import com.eviware.soapui.plugins.PluginConfiguration;
 @PluginConfiguration(groupId = "com.smartbear.ready.plugins",
         name = "Ready! API HTTP Archive REST Discovery Plugin", version = "1.0",
         description = "Use an HTTP Archive in REST Discovery",
-        infoUrl = "https://github.com/smartbear/")
+        infoUrl = "https://github.com/joeljons/ready-api-discover-har")
 public class PluginConfig extends PluginAdapter {
 }
