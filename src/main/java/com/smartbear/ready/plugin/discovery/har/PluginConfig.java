aeecdc133450728b25b66fd2d7c2b29d183cedbb
Joel Jonsson <joel.jonsson@smartbear.com>
2016-05-09 15:39:06 +0200

    Sync pom and PluginConfiguration version

--- src/main/java/com/smartbear/ready/plugin/discovery/har/PluginConfig.java
+++ src/main/java/com/smartbear/ready/plugin/discovery/har/PluginConfig.java
@@ -4,7 +4,7 @@ import com.eviware.soapui.plugins.PluginAdapter;
 import com.eviware.soapui.plugins.PluginConfiguration;
 
 @PluginConfiguration(groupId = "com.smartbear.ready.plugins",
-        name = "Ready! API HTTP Archive REST Discovery Plugin", version = "1.0",
+        name = "Ready! API HTTP Archive REST Discovery Plugin", version = "1.0.2",
         description = "Use an HTTP Archive in REST Discovery",
         infoUrl = "https://github.com/joeljons/ready-api-discover-har")
 public class PluginConfig extends PluginAdapter {
