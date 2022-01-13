5d5197abeb78d96d7396bde1accc145c934e3054
Alexey Bortulev <alexey.bortulev@smartbear.com>
2022-01-13 17:55:51 +0300

    RIA-17951 Set minimum ReadyAPI version

--- src/main/java/com/smartbear/ready/plugin/discovery/har/PluginConfig.java
+++ src/main/java/com/smartbear/ready/plugin/discovery/har/PluginConfig.java
@@ -6,6 +6,7 @@ import com.eviware.soapui.plugins.PluginConfiguration;
 @PluginConfiguration(groupId = "com.smartbear.ready.plugins",
         name = "HTTP Archive REST Discovery Plugin", version = "1.0.3-SNAPSHOT",
         description = "Use an HTTP Archive (HAR) in REST Discovery",
-        infoUrl = "https://github.com/SmartBear/ready-api-discover-har")
+        infoUrl = "https://github.com/SmartBear/ready-api-discover-har",
+        minimumReadyApiVersion="3.0.0")
 public class PluginConfig extends PluginAdapter {
 }
