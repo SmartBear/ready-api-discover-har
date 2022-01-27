cd5f940411f8f4533e030a363845fac1deeec94f
Alexey Bortulev <alexey.bortulev@smartbear.com>
2022-01-27 10:21:37 +0300

    RIA-18995 Disable for SoapUI OS

--- src/main/java/com/smartbear/ready/plugin/discovery/har/PluginConfig.java
+++ src/main/java/com/smartbear/ready/plugin/discovery/har/PluginConfig.java
@@ -7,6 +7,7 @@ import com.eviware.soapui.plugins.PluginConfiguration;
         name = "HTTP Archive REST Discovery Plugin", version = "1.0.3-SNAPSHOT",
         description = "Use an HTTP Archive (HAR) in REST Discovery",
         infoUrl = "https://github.com/SmartBear/ready-api-discover-har",
+        minimumSoapUIVersion = "99.0.0",
         minimumReadyApiVersion="3.20.1")
 public class PluginConfig extends PluginAdapter {
 }
