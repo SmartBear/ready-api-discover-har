e37ae8fb9842512ee0f1755439c725ea7ebd322d
KarolinaNogacka <karolina.nogacka@smartbear.com>
2025-12-22 11:43:13 +0100

    RIA-25868 Java 21 update (#13)
    * RIA-25868 java 21 update
    
    * RIA-25868 update maven plugins

--- src/main/java/com/smartbear/ready/plugin/discovery/har/PluginConfig.java
+++ src/main/java/com/smartbear/ready/plugin/discovery/har/PluginConfig.java
@@ -4,10 +4,10 @@ import com.eviware.soapui.plugins.PluginAdapter;
 import com.eviware.soapui.plugins.PluginConfiguration;
 
 @PluginConfiguration(groupId = "com.smartbear.ready.plugins",
-        name = "HTTP Archive REST Discovery Plugin", version = "1.0.4",
+        name = "HTTP Archive REST Discovery Plugin", version = "1.0.5",
         description = "Use an HTTP Archive (HAR) in REST Discovery",
         infoUrl = "https://github.com/SmartBear/ready-api-discover-har",
         minimumSoapUIVersion = "99.0.0",
-        minimumReadyApiVersion="3.52.0")
+        minimumReadyApiVersion="3.64.0")
 public class PluginConfig extends PluginAdapter {
 }
